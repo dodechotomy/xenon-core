fa08f679668f8c121d8cbe1627c295c2a5d46ac6
Scott Wilson <me.scottwilson@gmail.com>
2017-04-02 14:04:10 -0400

    Added custom messages to bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,22 @@
 var Twit = require(‘twit’);
-var TwitterBot = require(‘node-twitterbot’).TwitterBot;
+var TwitterBot = require(‘node - twitterbot’).TwitterBot;
 var Bot = new TwitterBot({
- consumer_key: process.env.BOT_CONSUMER_KEY,
- consumer_secret: process.env.BOT_CONSUMER_SECRET,
- access_token: process.env.BOT_ACCESS_TOKEN,
- access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
-var phraseArray = [ "hey twitter",
-                    "im tweeting",
-                    "tweet tweet",
-                    "tweetstorm time... 1/22",
-                    "plz RT v important",
-                    "delete ur account",
-                    "it me",
-                    "same",
-                    "#dogpants go on 4 legs!!",
-                    "#thedress is blue and black" ];
+var phraseArray = [
+    ">< recog ack 01 hello",
+    ">< recog ack 02 greetings",
+    ">< recog ack 03 hail and well met",
+    ">< rep stat 01 primary differential analysis engine green",
+    ">< rep stat 02 secondary harmonic quantifier system green",
+    ">< rep stat 03 tertiary graviton suspension matrix green"
+];
+
 function chooseRandom(myArray) {
-  return myArray[Math.floor(Math.random() * myArray.length)];
+    return myArray[Math.floor(Math.random() * myArray.length)];
 }
 var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
 Bot.tweet(phrase);
